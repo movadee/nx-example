026b1c68b2ca47923cf039c7176314d7fba62ae1
EL <elicloudh@gmail.com>
2019-07-16 19:43:47 +0300

    move comment component

--- apps/docu-ui/src/app/portal.module.ts
+++ apps/docu-ui/src/app/portal.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { SharedComponentsModule } from '@bmo/shared-components';
 
-import { AddCommentComponent } from './components/add-comment/add-comment.component';
-import { SharedStoreModule } from '@bmo/shared-store';
-
 @NgModule({
-  declarations: [AppComponent, AddCommentComponent],
+  declarations: [AppComponent],
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -19,7 +13,6 @@ import { SharedStoreModule } from '@bmo/shared-store';
       }
     ]),
     // RouterModule.forChild([]),
-    SharedStoreModule,
     SharedComponentsModule,
   ],
   bootstrap: [AppComponent]
